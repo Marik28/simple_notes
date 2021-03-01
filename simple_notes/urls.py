3eec5abd88b551c82d406b18453feb8159ef227b
Marik28 <markel20002000@gmail.com>
2021-03-02 00:44:16 +0600

    added yasg for documenting API

--- simple_notes/urls.py
+++ simple_notes/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url
-
+from django.conf import settings
 from . import views
-
+from .yasg import url_patterns as doc_urls
 
 urlpatterns = [
     url(r'^$', views.NoteListView.as_view(), name="notes"),
     url(r'^(?P<id>[\w-]+)$', views.NoteDetailView.as_view(), name="note_detail"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += doc_urls
